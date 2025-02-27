02481593837142e24f34ec64adf9fb673cfa8b4c
iPowerLife <99628430+iPowerLife@users.noreply.github.com>
2025-02-27 06:27:55 +0300

    123
    123

--- v0-user-next.config.js
+++ v0-user-next.config.js
@@ -5,7 +5,6 @@ const nextConfig = {
     NEXT_PUBLIC_SUPABASE_URL: process.env.SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_KEY: process.env.SUPABASE_ANON_KEY,
   },
-  // Настройки для Railway
   output: "standalone",
 }
 
