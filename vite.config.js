d0c5e582971f9701b32c41fd918cd2ad0a577b88
iPowerLife <99628430+iPowerLife@users.noreply.github.com>
2025-02-27 08:16:42 +0300

    12
    12

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,15 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: process.env.PORT || 3000,
+    host: true, // Добавляем для работы в Railway
   },
   build: {
     outDir: "dist",
     sourcemap: true,
+    // Указываем точку входа явно
+    rollupOptions: {
+      input: "/index.html",
+    },
   },
 })
 
