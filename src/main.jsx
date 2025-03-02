2ad6ba1ad711fcb8d10a8a545cd6161bc9f03d2f
iPowerLife <99628430+iPowerLife@users.noreply.github.com>
2025-03-02 17:36:13 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,11 @@ import "./styles.css" // Добавляем импорт стилей
 
 const root = createRoot(document.getElementById("root"))
 root.render(<App />)
+
+
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
 
