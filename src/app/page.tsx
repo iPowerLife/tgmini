7be43acc6fdab7a8081b660756b5cd7b2158678f
iPowerLife <99628430+iPowerLife@users.noreply.github.com>
2025-03-03 09:50:46 +0300

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import TasksSection from "../components/tasks-section"
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <TasksSection />
+    </main>
+  )
+}
+
