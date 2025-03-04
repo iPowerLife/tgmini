3345d18c0774b53b6ea7567572c189b02be0ad92
iPowerLife <99628430+iPowerLife@users.noreply.github.com>
2025-03-04 09:51:13 +0300

    234

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client"
-import App from "./App"
-import "./styles.css" // Добавляем импорт стилей
-
-const root = createRoot(document.getElementById("root"))
-root.render(<App />)
-
-
