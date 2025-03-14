a1c7d8b21ac227a747e5c556bd5c24c2609cb0c3
iPowerLife <99628430+iPowerLife@users.noreply.github.com>
2025-03-14 23:34:33 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import TasksPage from "./pages/tasks"
 import RatingPage from "./pages/rating-page"
 import ProfilePage from "./pages/profile-page"
 import { BottomMenu } from "./components/bottom-menu"
-import LoadingScreen from "./components/loading-screen"
+import { LoadingScreen } from "./components/loading-screen"
 
 function App() {
   const [session, setSession] = useState(null)
